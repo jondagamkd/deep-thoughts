44efad9847e78eeaccd58eebfc626d794f7a52e6
jondagamkd <johntaylorwoodland@gmail.com>
2021-12-09 16:22:53 -0800

    schemas

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { ApolloServerPluginLandingPageGraphQLPlayground } = require("apollo-server-core");
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
@@ -15,6 +16,7 @@ const startServer = async () => {
   const server = new ApolloServer({ 
     typeDefs, 
     resolvers,
+    context: authMiddleware,
     plugins: [
       ApolloServerPluginLandingPageGraphQLPlayground(),
     ]
