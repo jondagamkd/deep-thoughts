b57b74f7b92c5107246828acfa3b22cbd87370e1
jondagamkd <johntaylorwoodland@gmail.com>
2021-12-11 09:33:58 -0800

    front end and mern

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const { ApolloServerPluginLandingPageGraphQLPlayground } = require("apollo-serve
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
+const path = require('path');
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
@@ -37,6 +38,15 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
