55006a9c32feb176237f023c961fc2e6b93b6757
jondagamkd <johntaylorwoodland@gmail.com>
2021-12-23 16:02:22 -0800

    added thoughts

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,6 +4,7 @@ import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
 import ThoughtList from '../components/ThoughtList';
 import Auth from '../utils/auth';
 import FriendList from '../components/FriendList';
+import ThoughtForm from '../components/ThoughtForm';
 
 
 const Home = () => {
@@ -13,10 +14,16 @@ const Home = () => {
   const { data: userData } = useQuery(QUERY_ME_BASIC);
   const thoughts = data?.thoughts || [];
   const loggedIn = Auth.loggedIn();
+  
 
   return (
     <main>
       <div className="flex-row justify-space-between">
+        {loggedIn && (
+        <div className="col-12 mb-3">
+          <ThoughtForm />
+        </div>
+         )}
         <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
           {loading ? (
             <div>Loading...</div>
